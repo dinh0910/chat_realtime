15f0ead8622ffd69f0dbad973a787a5e928d9915
dinhpoor0910 <dinhpoor0910@gmail.com>
2025-02-01 15:31:38 +0700

    Update Client

---
+++ client/src/routes/sections/auth.jsx
@@ -0,0 +1,47 @@
+import { lazy, Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { AuthCenteredLayout } from 'src/layouts/auth-centered';
+
+import { SplashScreen } from 'src/components/loading-screen';
+
+/** **************************************
+ * Centered layout
+ *************************************** */
+const CenteredLayout = {
+  SignInPage: lazy(() => import('src/pages/auth/centered/sign-in')),
+  SignUpPage: lazy(() => import('src/pages/auth/centered/sign-up')),
+  VerifyPage: lazy(() => import('src/pages/auth/centered/verify')),
+  ResetPasswordPage: lazy(() => import('src/pages/auth/centered/reset-password')),
+  UpdatePasswordPage: lazy(() => import('src/pages/auth/centered/update-password')),
+};
+
+const authCentered = {
+  path: 'centered',
+  element: (
+    <AuthCenteredLayout>
+      <Outlet />
+    </AuthCenteredLayout>
+  ),
+  children: [
+    { path: 'sign-in', element: <CenteredLayout.SignInPage /> },
+    { path: 'sign-up', element: <CenteredLayout.SignUpPage /> },
+    { path: 'verify', element: <CenteredLayout.VerifyPage /> },
+    { path: 'reset-password', element: <CenteredLayout.ResetPasswordPage /> },
+    { path: 'update-password', element: <CenteredLayout.UpdatePasswordPage /> },
+  ],
+};
+
+// ----------------------------------------------------------------------
+
+export const authDemoRoutes = [
+  {
+    path: 'auth-demo',
+    element: (
+      <Suspense fallback={<SplashScreen />}>
+        <Outlet />
+      </Suspense>
+    ),
+    children: [authSplit, authCentered],
+  },
+];
